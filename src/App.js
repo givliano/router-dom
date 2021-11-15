f6eb634368a77a2e6a0ddc591a62ceec2626df2d
Giuliano Machado <giufma@gmail.com>
2021-11-15 13:31:38 -0300

    add initial routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Link } from 'react-router-dom';
+
 export default function App() {
   return (
     <div>
       <h1>Bookkepper!</h1>
+
+      <nav style={{
+        borderBottom: 'solid 1px',
+        paddingBottom: '1rem'
+      }}>
+        <Link to="/invoices">Invoices</Link> | {" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import { render } from 'react-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
 import App from './App';
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
 
 const rootElement = document.getElementById('root');
-render(<App />, rootElement);
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
