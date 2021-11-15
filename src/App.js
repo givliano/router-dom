a647e14636ee58dbd7a2a60d6055086ee4f08334
Giuliano Machado <giufma@gmail.com>
2021-11-15 18:54:32 -0300

    add outlet component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
-      <h1>Bookkepper!</h1>
-
+      <h1>Bookkeeper</h1>
       <nav style={{
-        borderBottom: 'solid 1px',
-        paddingBottom: '1rem'
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem"
       }}>
         <Link to="/invoices">Invoices</Link> | {" "}
         <Link to="/expenses">Expenses</Link>
       </nav>
+      {/*
+        It nests the URLs ("/" + "expenses" and "/" + "invoices")
+        It will nest the UI components for shared layout when the child route matches:
+      */}
+      <Outlet />
     </div>
   );
 }
